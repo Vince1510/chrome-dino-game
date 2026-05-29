8977928a44a8b8c52ab4424d37f6a0d2067871e1
Vince van Apeldoorn <vincevanapeldoorn@gmail.com>
2026-05-29 15:09:47 +0200

    werkt die nu wel

--- src/resources.ts
+++ src/resources.ts
@@ -1,14 +1,14 @@
 import { ImageSource, Loader, Resource } from "excalibur";
 
 const Resources = {
-  pacmanClosed: new ImageSource("/images/pacman/1.png"),
-  pacmanHalf: new ImageSource("/images/pacman/2.png"),
-  pacmanOpen: new ImageSource("/images/pacman/3.png"),
+  pacmanClosed: new ImageSource("./images/pacman/1.png"),
+  pacmanHalf: new ImageSource("./images/pacman/2.png"),
+  pacmanOpen: new ImageSource("./images/pacman/3.png"),
 
-  blinky: new ImageSource("/images/ghosts/blinky.png"),
-  pinky: new ImageSource("/images/ghosts/pinky.png"),
-  inky: new ImageSource("/images/ghosts/inky.png"),
-  clyde: new ImageSource("/images/ghosts/clyde.png"),
+  blinky: new ImageSource("./images/ghosts/blinky.png"),
+  pinky: new ImageSource("./images/ghosts/pinky.png"),
+  inky: new ImageSource("./images/ghosts/inky.png"),
+  clyde: new ImageSource("./images/ghosts/clyde.png"),
 };
 
 const ResourceLoader = new Loader();
